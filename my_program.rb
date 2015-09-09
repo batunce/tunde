bd133394a19657c9f610d2ef2d871c6cebf6fbe4
Tunde <tadebab@yahoo.com>
2015-09-09 12:12:41 +0100

    new file added

---
+++ my_program.rb
@@ -0,0 +1,13 @@
+class Sample
+def hello
+puts "Good morning Mr Tunde, how was your night?"
+a=5+10
+puts a
+end
+end
+s=Sample.new
+s.hello
+5.times do |a|
+puts "What's up, mehn?" 
+end
+7.times{|b|puts "#{b} :I am learning Ruby"}
